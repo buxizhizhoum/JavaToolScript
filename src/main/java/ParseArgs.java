ae822cfb8056055193326bbd2d4e3c0e50a993ef
buxizhizhoum <mapeaks@126.com>
2018-07-27 15:39:38 +0800

    Add ParseArgs.java

---
+++ src/main/java/ParseArgs.java
@@ -0,0 +1,44 @@
+/**
+ * from https://stackoverflow.com/a/367714/8025086
+ */
+
+import org.apache.commons.cli.*;
+
+public class ParseArgs {
+    public static void main(String[] args) throws Exception{
+        parseArgs(args);
+    }
+
+    public static void parseArgs(String[] args) throws Exception {
+
+        Options options = new Options();
+
+        Option input = new Option("i", "input", true, "input file path");
+        input.setRequired(true);
+        options.addOption(input);
+
+        Option output = new Option("o", "output", true, "output file");
+        output.setRequired(true);
+        options.addOption(output);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("utility-name", options);
+
+            System.exit(1);
+        }
+
+        String inputFilePath = cmd.getOptionValue("input");
+        String outputFilePath = cmd.getOptionValue("output");
+
+        System.out.println(inputFilePath);
+        System.out.println(outputFilePath);
+
+    }
+}
